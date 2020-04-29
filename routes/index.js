5a0e87e927065831234b6b86f9e2a9178026b3f0
vancerweston <vweston@student.neumont.edu>
2020-04-29 14:00:01 -0600

    fixed routes, modified pages, started expanding structure of index.jade

--- routes/index.js
+++ routes/index.js
@@ -3,13 +3,33 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express Burgers' });
+  res.render('index', { 
+    title: 'Express Burgers',
+    aboutTitle: 'All About Us',
+    aboutUs: 'We have always loved the small American resturants that define our culinary culture. Nothing is better than a fresh grilled burger or a smooth shake on a sunday. We know you love these things, and we love them too. So, we set out to bring those things to you. From our kitchen to yours, fresh food at a great price.' 
+});
 });
 
 /* GET burgers page. */
 router.get('/burgers', function(req, res, next) {
   res.render('burgers', { title: 'Burgers' });
 });
+
+/* GET Sides page. */
+router.get('/sides', function(req, res, next) {
+  res.render('sides', { title: 'Sides' });
+});
+
+/* GET Desserts page. */
+router.get('/desserts', function(req, res, next) {
+  res.render('desserts', { title: 'Desserts' });
+});
+
+/* GET burgers page. */
+router.get('/directions', function(req, res, next) {
+  res.render('directions', { title: 'Directions' });
+});
+
 
 
 module.exports = router;
