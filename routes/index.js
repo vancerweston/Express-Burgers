fb08dee2151531b03086226974e9644f5248ca7e
vancerweston <vweston@student.neumont.edu>
2020-04-29 14:25:00 -0600

    added menu items to burgers, sides, and desserts

--- routes/index.js
+++ routes/index.js
@@ -7,25 +7,61 @@ router.get('/', function(req, res, next) {
     title: 'Express Burgers',
     aboutTitle: 'All About Us:',
     aboutBody: 'We have always loved the small American resturants that define our culinary culture. Nothing is better than a fresh grilled burger or a smooth shake on a sunday. We know you love these things, and we love them too. So, we set out to bring those things to you. From our kitchen to yours, fresh food at a great price.' 
-});
+  });
 });
 
 /* GET burgers page. */
 router.get('/burgers', function(req, res, next) {
-  res.render('burgers', { title: 'Burgers' });
+  res.render('burgers', { 
+    title: 'Burgers',
+    burgerOne: 'Burger1Name',
+    burgerTwo: 'Burger2Name',
+    burgerThree: 'Burger3Name',
+    burgerFour: 'Burger4Name',
+    burgerFive: 'Burger5Name',
+    descOne: 'description...',
+    descTwo: 'description...',
+    descThree: 'description...',
+    descFour: 'description...',
+    descFive: 'description...'
+  });
 });
 
 /* GET Sides page. */
 router.get('/sides', function(req, res, next) {
-  res.render('sides', { title: 'Sides' });
+  res.render('sides', { 
+    title: 'Sides',
+    sideOne: 'side1Name',
+    sideTwo: 'side2Name',
+    sideThree: 'side3Name',
+    sideFour: 'side4Name',
+    sideFive: 'side5Name',
+    descOne: 'description...',
+    descTwo: 'description...',
+    descThree: 'description...',
+    descFour: 'description...',
+    descFive: 'description...' 
+  });
 });
 
 /* GET Desserts page. */
 router.get('/desserts', function(req, res, next) {
-  res.render('desserts', { title: 'Desserts' });
+  res.render('desserts', { 
+    title: 'Desserts',
+    dessertOne: 'dessert1Name',
+    dessertTwo: 'dessert2Name',
+    dessertThree: 'dessert3Name',
+    dessertFour: 'dessert4Name',
+    dessertFive: 'dessert5Name',
+    descOne: 'description...',
+    descTwo: 'description...',
+    descThree: 'description...',
+    descFour: 'description...',
+    descFive: 'description...'
+  });
 });
 
-/* GET burgers page. */
+/* GET Directions page. */
 router.get('/directions', function(req, res, next) {
   res.render('directions', { title: 'Directions' });
 });
