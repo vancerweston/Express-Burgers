2bf95d3588da2fa0055836077abe51bc98a0bc1a
vancerweston <vweston@student.neumont.edu>
2020-04-30 13:14:59 -0600

    working on api call for map

--- public/scripts/directions.js
+++ public/scripts/directions.js
@@ -1,18 +1,31 @@
-let googleMap;
-const url = '#';
-const request = new XMLHttpRequest();
+// let googleMap;
+// const url = '#';
+// const request = new XMLHttpRequest();
 
-function loadData() {
-    request.open('GET', url);
-    request.onload = loadComplete;
-    request.send();
-}
+// function loadData() {
+//     request.open('GET', url);
+//     request.onload = loadComplete;
+//     request.send();
+// }
 
-function loadComplete(evt) {
-    googleMap = JSON.parse(request.responseText);
-    console.log(googleMap);
+// function loadComplete(evt) {
+//     googleMap = JSON.parse(request.responseText);
+//     console.log(googleMap);
 
-    //Put JSON Object data into directions.jade
-}
+//     //Put JSON Object data into directions.jade
+// }
 
-loadData();
+// loadData();
+
+var map = new ol.Map({
+    target: 'map',
+    layers: [
+      new ol.layer.Tile({
+        source: new ol.source.OSM()
+      })
+    ],
+    view: new ol.View({
+      center: ol.proj.fromLonLat([37.41, 8.82]),
+      zoom: 4
+    })
+  });
